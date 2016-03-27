6c75682513bba7c18466fd9818a7d916c5737247
atjuncarol <atjuncarol@gmail.con>
2016-03-27 14:19:01 +0000

    user-profile-27032016

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :profile, :region, :password, :password_confirmation)
   end
   
   def user_profile
