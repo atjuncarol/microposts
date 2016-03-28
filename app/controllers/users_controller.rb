59c38d187350c277f7883723d3859b9b7fbf00d0
atjuncarol <atjuncarol@gmail.con>
2016-03-28 14:13:02 +0000

    28032016_3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show, :edit, :update]
-  before_action :logged_in_user, only:[:show, :edit, :update]
+  before_action :logged_in_user, only:[:edit, :update]
   before_action :authenticate!, only:[:edit, :update]
   
   def show
