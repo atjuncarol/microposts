c11570d4c5d629bc9cceb446dc8f01b480b495e6
atjuncarol <atjuncarol@gmail.con>
2016-03-31 08:18:37 +0000

    31032016

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
                                     dependent:   :destroy
   has_many :follower_users, through: :follower_relationships, source: :follower
   
+  
   # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.find_or_create_by(followed_id: other_user.id)
@@ -34,4 +35,5 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+ 
 end
