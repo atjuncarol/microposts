28864de809fe8938fe7034edd55126dd8ea074fe
atjuncarol <atjuncarol@gmail.con>
2016-04-01 14:39:25 +0000

    01042016_3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,9 +12,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # デフォルト値
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # public/mypath/ 配下にファイルが配置される
+    "mypath"
   end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
