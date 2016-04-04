e933864834e5d1bf9b23efdeedfc9ef85632d6f0
atjuncarol <atjuncarol@gmail.con>
2016-04-04 14:01:11 +0000

    04042016_heroku_2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @micropost = current_user.microposts.build
+     @micropost = current_user.microposts.build
      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+     @user = User.find(current_user)
+     @microposts = @user.microposts.order(created_at: :desc).paginate(page: params[:page], :per_page => 10)
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc)
+    # @microposts = @user.microposts.order(created_at: :desc)
+    @microposts = @user.microposts.paginate(page: params[:page], :per_page => 10)
   end
   
   def new
@@ -37,6 +38,7 @@ class UsersController < ApplicationController
   
   def index
     @users = User.paginate(page: params[:page], :per_page => 10)
+    
   end
   
   private
